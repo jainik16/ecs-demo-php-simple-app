6e739fd465800574ba751c70dc6c92cd8c855c84
JainikMehta <Jainik.Mehta@ibm.com>
2017-02-03 11:23:02 -0600

    back change

--- src/index.php
+++ src/index.php
@@ -7,7 +7,7 @@
         <title>Demo PHP App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-        <style>body {margin-top: 40px; background-color: #343;}</style>
+        <style>body {margin-top: 40px; background-color: #346;}</style>
         <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
         <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     </head>
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>Demo PHP App</h1>
-                <h2>IBM THINK </h2>
+                <h2>IBM THINK  BLUE</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
